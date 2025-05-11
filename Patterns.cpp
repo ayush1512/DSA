b274c1c8e523255e4cf653c91e7d7dca64da2a09
Ayush Saxena <ayushsaxena1512@gmail.com>
2025-05-11 12:40:48 +0000

    added Patterns 7-10

--- Patterns.cpp
+++ Patterns.cpp
@@ -11,6 +11,7 @@ int main(){
 
 
     // Pattern 1;
+    
     int n=5;
     // cin >> n;
     for(int i=0; i<n; i++){
@@ -71,5 +72,90 @@ int main(){
         cout << endl;
     }
     cout << endl;
+
+    // Pattern 7;
+
+    for(int i=0; i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout << " ";
+        }
+        for(int j=0;j<2*i+1;j++){
+            cout << "x";
+        }
+        for(int j=0;j<n-i-1;j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    // Pattern 8;
+
+    for(int i=0; i<n;i++){
+        for(int j=0;j<i;j++){
+            cout << " ";
+        }
+        for(int j=0;j<2*n-2*i-1;j++){
+            cout << "x";
+        }
+        for(int j=0;j<i;j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    // Pattern 9;
+
+    for(int i=0; i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout << " ";
+        }
+        for(int j=0;j<2*i+1;j++){
+            cout << "x";
+        }
+        for(int j=0;j<n-i-1;j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+    for(int i=0; i<n;i++){
+        for(int j=0;j<i;j++){
+            cout << " ";
+        }
+        for(int j=0;j<2*n-2*i-1;j++){
+            cout << "x";
+        }
+        for(int j=0;j<i;j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    // Pattern 10;
+
+    for(int i=0; i<n-1;i++){
+        for(int j=0;j<i;j++){
+            cout << "*";
+        }
+        for(int j=0;j<n-i-1;j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+    for(int i=0; i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout << "*";
+        }
+        for(int j=0;j<i;j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    
 
 }
