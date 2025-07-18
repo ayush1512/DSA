d4f0ce4fe20896c923f56e3cc82464163387fb61
Ayush Saxena <ayushsaxena1512@gmail.com>
2025-07-18 15:09:23 +0000

    commit

--- Arrays/longestSubArr/brute.cpp
+++ Arrays/longestSubArr/brute.cpp
@@ -1,17 +1,24 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-int main(){
-    int n,k;
+int main() {
+    int n, k;
     cin >> n;
-    vector<int> arr(n);
+    vector < int > arr(n);
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
     cin >> k;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; i++){
-            
+    int len = 0;
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = 0; j < n; j++) {
+            sum += arr[j];
+            if (sum == k) {
+                len = max(len, j - i + 1);
+            }
         }
     }
+    cout << len << endl;
 }
